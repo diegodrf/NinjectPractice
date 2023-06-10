e80025149acdff336e9d54427e4c1610f7a7e442
Diego Faria <diego.rdfaria@gmail.com>
2023-06-10 02:22:17 -0400

    New Custom Exception

---
+++ NinjectPractice/Exceptions/ElementNotFound.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NinjectPractice.Exceptions
+{
+    public class ElementNotFound : Exception
+    {
+        public ElementNotFound(string message) : base(message) {}
+    }
+}
