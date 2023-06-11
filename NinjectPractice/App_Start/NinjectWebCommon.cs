4faaed47f8caafb5bcbd43a25544a30a07161c65
Diego Faria <diego.rdfaria@gmail.com>
2023-06-11 02:50:28 -0400

    Minor fixs

--- NinjectPractice/App_Start/NinjectWebCommon.cs
+++ NinjectPractice/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,6 @@ namespace NinjectPractice.App_Start
         /// Load your modules or register your services here!
         /// </summary>
         /// <param name="kernel">The kernel.</param>
-        private static void RegisterServices(IKernel kernel) { }
+        private static void RegisterServices(IKernel kernel) {}
     }
 }
