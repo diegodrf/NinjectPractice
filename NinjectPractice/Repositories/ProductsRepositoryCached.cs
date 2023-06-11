92074cd842bc6672ece9dd3df8f01033037a9b51
Diego Faria <diego.rdfaria@gmail.com>
2023-06-11 02:48:09 -0400

    Implement memory cache

---
+++ NinjectPractice/Repositories/ProductsRepositoryCached.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using NinjectPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NinjectPractice.Repositories
+{
+    public class ProductsRepositoryCached : IProductsRepository
+    {
+        private readonly IProductsRepository _productsRepository;
+        private readonly IMemoryCache _memoryCache;
+        private readonly TimeSpan TIME_TO_EXPIRE_CACHE = TimeSpan.FromMinutes(5);
+
+        public ProductsRepositoryCached(IProductsRepository productsRepository, IMemoryCache memoryCache)
+        {
+            _productsRepository = productsRepository;
+            _memoryCache = memoryCache;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            return await _productsRepository.CreateAsync(product);
+        }
+
+        public async Task<Product> UpdateAsync(int id, Product product)
+        {
+            return await _productsRepository.UpdateAsync(id, product);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            var key = "AllProducts";
+            return await _memoryCache.GetOrCreateAsync(key, async options =>
+            {
+                options.AbsoluteExpirationRelativeToNow = TIME_TO_EXPIRE_CACHE;
+                return await _productsRepository.GetAllAsync();
+            });
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            var key = $"Product{id}";
+            return await _memoryCache.GetOrCreateAsync(key, async options =>
+            {
+                options.AbsoluteExpirationRelativeToNow = TIME_TO_EXPIRE_CACHE;
+                return await _productsRepository.GetByIdAsync(id);
+            });
+        }
+
+
+    }
+}
