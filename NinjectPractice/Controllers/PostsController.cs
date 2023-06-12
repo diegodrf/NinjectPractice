7aa644fbbe6b629d25cd488ad556ed983a6c1226
Diego Faria <diego.rdfaria@gmail.com>
2023-06-11 23:34:25 -0400

    Add new Controller for posts

---
+++ NinjectPractice/Controllers/PostsController.cs
@@ -0,0 +1,32 @@
+﻿using NinjectPractice.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace NinjectPractice.Controllers
+{
+    [RoutePrefix("api/posts")]
+    public class PostsController : ApiController
+    {
+        private readonly IJsonPlaceholderService _jsonPlaceholderService;
+
+        public PostsController(IJsonPlaceholderService jsonPlaceholderService)
+        {
+            _jsonPlaceholderService = jsonPlaceholderService;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public async Task<IEnumerable<Post>> GetAll()
+        {
+            return await _jsonPlaceholderService.GetAllPostAsync();
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public async Task<Post> GetById(int id)
+        {
+            return await _jsonPlaceholderService.GetPostById(id);
+        }
+    }
+}
