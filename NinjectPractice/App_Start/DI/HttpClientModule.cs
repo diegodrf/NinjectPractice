176c97c7114debbd5a7bb82f568def986c511e78
Diego Faria <diego.rdfaria@gmail.com>
2023-06-11 23:33:29 -0400

    Add HttpClient

---
+++ NinjectPractice/App_Start/DI/HttpClientModule.cs
@@ -0,0 +1,24 @@
+﻿using Ninject.Modules;
+using System;
+using System.Net.Http;
+
+namespace NinjectPractice.App_Start.DI
+{
+    public class HttpClientModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<HttpClient>()
+                .ToMethod(context =>
+                {
+                    var client = new HttpClient
+                    {
+                        BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
+                    };
+                    client.DefaultRequestHeaders.Clear();
+                    return client;
+                })
+                .InSingletonScope();
+        }
+    }
+}
