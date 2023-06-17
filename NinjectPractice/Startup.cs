8f6906b1cf7783798f3eb3f9cc6153ad6b0d6190
Diego Faria <diego.rdfaria@gmail.com>
2023-06-17 11:33:40 -0400

    Create Startup for middleware

---
+++ NinjectPractice/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Ninject;
+using Ninject.Modules;
+using NinjectPractice.App_Start.DI;
+using NinjectPractice.Middlewares;
+using Owin;
+
+[assembly: OwinStartup(typeof(NinjectPractice.Startup))]
+
+namespace NinjectPractice
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var kernel = new StandardKernel(BindModules());
+            
+            app.UseNinject(() => kernel);
+
+            app.Use(typeof(ErrorHandlerMiddleware));
+            app.Use(typeof(RequestIdGeneratorMiddleware));
+            
+        }
+
+        private INinjectModule[] BindModules()
+        {
+            var modules = new INinjectModule[]
+            {
+                new DbContextModule(),
+                new RepositoriesModule(),
+                new HttpClientModule()
+            };
+            return modules;
+        }
+    }
+}
