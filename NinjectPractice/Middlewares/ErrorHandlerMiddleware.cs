14186e3b80db262c074f934670fc9ce4d4a29594
Diego Faria <diego.rdfaria@gmail.com>
2023-06-17 11:33:06 -0400

    Create Middlewares

---
+++ NinjectPractice/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NinjectPractice.Middlewares
+{
+    public class ErrorHandlerMiddleware : OwinMiddleware
+    {
+        public ErrorHandlerMiddleware(OwinMiddleware next) : base(next) { }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            try
+            {
+                await Next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                // LOG
+            }
+
+        }
+    }
+}
---
+++ NinjectPractice/Middlewares/RequestIdGeneratorMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NinjectPractice.Middlewares
+{
+    public class RequestIdGeneratorMiddleware : OwinMiddleware
+    {
+        private readonly string _requestId;
+
+        public RequestIdGeneratorMiddleware(OwinMiddleware next) : base(next)
+        {
+            _requestId = Guid.NewGuid().ToString("D");
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            var requestIdHeader = new KeyValuePair<string, string[]>("RequestId", new string[] { _requestId });
+
+            context.Request.Headers.Add(requestIdHeader);
+
+            await Next.Invoke(context);
+
+            context.Response.Headers.Add(requestIdHeader);
+        }
+    }
+}
